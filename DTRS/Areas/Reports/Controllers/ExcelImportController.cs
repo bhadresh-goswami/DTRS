231433104828c2eddc794c4c693a514de15c804c
fifiwu <70978017+fifiwu@users.noreply.github.com>
2020-10-24 05:41:08 +0530

    add ExcelImportController

---
+++ DTRS/Areas/Reports/Controllers/ExcelImportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DTRS.Areas.Reports.Controllers
+{
+    public class ExcelImportController : Controller
+    {
+        // GET: Reports/ExcelImport
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
